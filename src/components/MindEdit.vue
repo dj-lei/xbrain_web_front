e53421dc9d63aac3d5f1ca962b07cc89393b4bad
dj-lei <m18349125880@gmail.com>
2021-03-09 10:55:17 +0800

    fix exception

--- src/components/MindEdit.vue
+++ src/components/MindEdit.vue
@@ -39,7 +39,7 @@
                   v-spacer
             v-data-table(:headers="headers", :items="data", class="elevation-1")
               template(v-slot:item.Status="{ item }")
-                v-slider.caption(v-model="item.Status" :tick-labels="ticksLabels" :max="3" step="1" tick-size="4")
+                v-slider(v-model="item.Status" :tick-labels="ticksLabels" :max="3" step="1" tick-size="4")
                 //- v-chip(:color="getColor(item.Status)", dark) {{ item.Status }}
               template(v-slot:item.Schedule="{ item }")
                 v-slider(v-model="item.Schedule" :thumb-size="24" thumb-label="always")
